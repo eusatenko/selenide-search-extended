16083d06070e104edfc83260cb4bf7ae38cb974f
akstas84 <akstas84@gmail.com>
2021-07-31 13:12:43 +0300

    add TestData

--- src/test/java/ru/akstas84/qa/TestBoxTests.java
+++ src/test/java/ru/akstas84/qa/TestBoxTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.akstas84.qa.Page.PracticePage;
+import static Data.TestData.*;
 
 public class TestBoxTests {
 
@@ -18,10 +19,9 @@ public class TestBoxTests {
   @Test
   void positiveFillTestPracticeFormTest() {
     practicePage.openPage();
-    practicePage.typeRregistrationDataAndSubmit("St", "Syn", "st@qa.ru", "Male", "9011231232", "some street 1");
+    practicePage.typeRregistrationDataAndSubmit(firstName,lastName, email, gender, phone, address );
     practicePage.checkResultsTitle();
-    practicePage.checkResultsValue("St", "Syn", "st@qa.ru", "Male", "9011231232", "some street 1");
-
+    practicePage.checkResultsValue(firstName,lastName, email, gender, phone, address);
   }
 
 }
